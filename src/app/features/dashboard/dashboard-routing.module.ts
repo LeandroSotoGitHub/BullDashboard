1ea06d23d72947cd096bcb66f6bcc6627bb1f48e
Leandro Soto <soto.lean@gmail.com>
2024-12-19 15:11:16 -0300

    MOD, Side nav

--- src/app/features/dashboard/dashboard-routing.module.ts
+++ src/app/features/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MedialContentModule } from './medial-content/medial-content.module';
 
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => 
       import('./home/home.module').then((m) => m.HomeModule)
+  },  
+  {
+    path: 'sales',
+    loadChildren: () => 
+      import('./sales/sales.module').then((m) => m.SalesModule)
+  },  
+  {
+    path: 'medial-content',
+    loadChildren: () => 
+      import('./medial-content/medial-content.module').then((m) => m.MedialContentModule)
+  },  
+  {
+    path: 'buy',
+    loadChildren: () => 
+      import('./buy/buy.module').then((m) => m.BuyModule)
   },  
   {
     path: '**',
--- src/app/features/dashboard/medial-content/medial-content-routing.module.ts
+++ src/app/features/dashboard/medial-content/medial-content-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MedialContentComponent } from './medial-content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:MedialContentComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
